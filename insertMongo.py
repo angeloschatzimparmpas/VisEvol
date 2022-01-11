36223569b29754ec43f1841c351a97537f545a46
Angelos Chatzimparmpas <angelos.chatzimparmpas@gmail.com>
2022-01-11 17:29:45 +0100

    Update insertMongo.py

--- insertMongo.py
+++ insertMongo.py
@@ -17,8 +17,8 @@ def import_content(filepath):
 
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
-    db_cm.remove()
-    db_cm.insert(data_json)
+    db_cm.drop()
+    db_cm.insert_many(data_json)
 
 if __name__ == "__main__":
   filepath = '/Users/anchaa/Documents/Research/VisEvol_code/extra_data_sets/iris.csv'
